6812723a588887d7082f6bc15599cfef51ca86cd
Devansh Gupta <devansh.gpt19@gmail.com>
2025-11-28 10:05:46 -0800

    fixed compilation errors in part2 code

--- systolic_array_part2/mac_array.v
+++ systolic_array_part2/mac_array.v
@@ -1,40 +1,42 @@
 // Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
 // Please do not spread this code without permission 
-module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid);
-
-  parameter bw = 4;
+module mac_array (clk, reset, out_s, in_w, in_n, inst_w, mode, valid);
+  parameter act_bw = 2;
+  parameter w_bw = 4;
   parameter psum_bw = 16;
   parameter col = 8;
   parameter row = 8;
 
   input  clk, reset;
-  output [psum_bw*col-1:0] out_s;
-  input  [row*bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+  output [2*psum_bw*col-1:0] out_s;
+  input  [row*w_bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
   input  [1:0] inst_w;
-  input  [psum_bw*col-1:0] in_n;
+  input  [2*psum_bw*col-1:0] in_n;
+  input mode; //0: 4-bit act and 4-bit weight;    1: 2-bit act and 4-bit weight
   output [col-1:0] valid;
 
 
   reg    [2*row-1:0] inst_w_temp;
-  wire   [psum_bw*col*(row+1)-1:0] temp;
+  wire   [2*psum_bw*col*(row+1)-1:0] temp;
   wire   [row*col-1:0] valid_temp;
 
 
   genvar i;
  
-  assign out_s = temp[psum_bw*col*9-1:psum_bw*col*8];
+  assign out_s = temp[2*psum_bw*col*9-1:2*psum_bw*col*8];
   assign temp[psum_bw*col*1-1:psum_bw*col*0] = 0;
   assign valid = valid_temp[row*col-1:row*col-8];
 
   for (i=1; i < row+1 ; i=i+1) begin : row_num
-      mac_row #(.bw(bw), .psum_bw(psum_bw)) mac_row_instance (
+      mac_row #(.act_bw(act_bw), .w_bw(w_bw), .psum_bw(psum_bw)) mac_row_instance (
         .clk(clk),
         .reset(reset),
-	      .in_w(in_w[bw*i-1:bw*(i-1)]),
+        .mode(mode),
+	      .in_w(in_w[w_bw*i-1:w_bw*(i-1)]),
 	      .inst_w(inst_w_temp[2*i-1:2*(i-1)]),
-	      .in_n(temp[psum_bw*col*i-1:psum_bw*col*(i-1)]),
+	      .in_n(temp[2*psum_bw*col*i-1:2*psum_bw*col*(i-1)]),
         .valid(valid_temp[col*i-1:col*(i-1)]),
-	      .out_s(temp[psum_bw*col*(i+1)-1:psum_bw*col*(i)]));
+	      .out_s(temp[2*psum_bw*col*(i+1)-1:2*psum_bw*col*(i)]));
   end
 
   always @ (posedge clk) begin
--- systolic_array_part2/mac_tile.v
+++ systolic_array_part2/mac_tile.v
@@ -3,45 +3,45 @@
 module mac_tile (clk, out_s, in_w, out_e, in_n, inst_w, inst_e, mode, reset);
 parameter act_bw = 2;
 parameter w_bw = 4;
-parameter psum_bw = 12;
+parameter psum_bw = 16;
 
-output [psum_bw-1:0] out_s[2];
+output [2*psum_bw-1:0] out_s;
 input  [w_bw-1:0] in_w;
 output [w_bw-1:0] out_e; 
-input  [1:0] inst_w;
+input  [1:0] inst_w;    // inst[1]:execute, inst[0]: kernel loading
 output [1:0] inst_e;
-input  [psum_bw-1:0] in_n[2];
+input  [2*psum_bw-1:0] in_n;
 input  clk;
 input mode; //0: 4-bit act and 4-bit weight;    1: 2-bit act and 4-bit weight
 input  reset;
 
 reg [1:0] inst_q;
-reg signed [act_bw-1:0] a_q[2]; // activation
-reg signed [w_bw-1:0] b_q[2]; // weight
-reg signed [psum_bw-1:0] c_q[2];
+reg signed [2*act_bw-1:0] a_q; // activation
+reg signed [2*w_bw-1:0] b_q; // weight
+reg signed [2*psum_bw-1:0] c_q;
 reg load_ready_q;
+reg cnt;
 
-wire signed [psum_bw-1:0] mac_out[2];
+wire signed [2*psum_bw-1:0] mac_out;
 
 assign out_e = a_q;
 assign inst_e = inst_q;
 
 mac #(.act_bw(act_bw), .w_bw(w_bw), .psum_bw(psum_bw)) mac_instance1 (
-        .a(a_q[0]), 
-        .b(b_q[0]),
-        .c(c_q[0]),
-	.out(mac_out[0])
+        .a(a_q[act_bw-1:0]), 
+        .b(b_q[w_bw-1:0]),
+        .c(c_q[psum_bw-1:0]),
+	.out(mac_out[psum_bw-1:0])
 );
 
 mac #(.act_bw(act_bw), .w_bw(w_bw), .psum_bw(psum_bw)) mac_instance2 (
-        .a(a_q[1]), 
-        .b(b_q[1]),
-        .c(c_q[1]),
-	.out(mac_out[1])
+        .a(a_q[2*act_bw-1:act_bw]), 
+        .b(b_q[2*w_bw-1:w_bw]),
+        .c(c_q[2*psum_bw-1:psum_bw]),
+	.out(mac_out[2*psum_bw-1:psum_bw])
 );
 
-assign out_s[0] = mac_out[0];
-assign out_s[1] = mac_out[1];
+assign out_s = mac_out;
 
 always @ (posedge clk) begin
     if (reset == 1) begin
@@ -49,37 +49,33 @@ always @ (posedge clk) begin
         load_ready_q <= 1'b1;
         cnt <= 1'b0;
         
-        a_q[0] <= 0;
-        b_q[0] <= 0;
-        c_q[0] <= 0;
-
-        a_q[1] <= 0;
-        b_q[1] <= 0;
-        c_q[1] <= 0;
+        a_q <= 0;
+        b_q <= 0;
+        c_q <= 0;
         
-    end else begin
-        
+    end
+    else begin    
         inst_q[1] <= inst_w[1];
 
         if (inst_w[0] | inst_w[1]) begin
-            a_q[0] <= in_w[1:0];
-            a_q[1] <= in_w[3:2];
+            a_q <= in_w;
         end
         
         if (inst_w[1]) begin
-            c_q[0] <= in_n[0];
-            c_q[1] <= in_n[1];
+            c_q <= in_n;
         end
 
         if (inst_w[0] == 1'b1 && load_ready_q == 1'b1) begin
             if (mode == 0) begin
-                b_q[0] <= in_w;
-                b_q[1] <= in_w;
+                b_q <= {in_w, in_w};
                 load_ready_q <= 1'b0;
             end
             else begin
-                b_q[cnt] <= in_w;
-                cnt = cnt + 1'b1;
+                case (cnt)
+                    1'b0: b_q[w_bw-1:0] <= in_w;
+                    1'b1: b_q[2*w_bw-1:w_bw] <= in_w;
+                endcase
+                cnt <= cnt + 1'b1;
                 if (cnt > 1) begin
                     load_ready_q <= 1'b0;
                 end
